baf678b0135010156e2f0db74599aec80480a7f8
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-12-05 11:42:15 -0300

    config.sh: Remove line 17

--- m23xq/config.sh
+++ m23xq/config.sh
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-
 # Device configuration file for Galaxy M23 5G (m23xq)
 TARGET_NAME="Galaxy M23 5G"
 TARGET_CODENAME="m23xq"
